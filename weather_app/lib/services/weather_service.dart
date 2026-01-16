fb9d92dfd6fb2d850363da347a804cde3b837d94
Bcrystelle <168496437+Bcrystelle@users.noreply.github.com>
2026-01-16 10:35:45 +0800

    lab 1 Flutter weather application
    Updated API key to a placeholder for security reasons.

--- weather_app/lib/services/weather_service.dart
+++ weather_app/lib/services/weather_service.dart
@@ -3,7 +3,7 @@ import 'dart:convert';
 import '../models/weather.dart';
 
 class WeatherService {
-  static const String apiKey = '9d8f7032e968b25e7f01a894a061d9b4';
+  static const String apiKey = '----';
   static const String baseUrl ='https://api.openweathermap.org/data/2.5/weather';
 
   static Future<Weather> getWeather(String cityName) async {
